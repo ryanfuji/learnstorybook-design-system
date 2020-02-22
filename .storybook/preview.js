65589e2c84e257e4cf06f7119d70f162363a8a95
Ryan Fujiwara <ryanfuji@Ryans-iMac.local>
2020-02-22 14:52:36 -0600

    final commit on testing this project

---
+++ .storybook/preview.js
@@ -0,0 +1,12 @@
+/** @format */
+
+import React from 'react';
+import { addDecorator } from '@storybook/react';
+import { GlobalStyle } from '../src/shared/global';
+
+addDecorator((story) => (
+	<>
+		<GlobalStyle />
+		{story()}
+	</>
+));
